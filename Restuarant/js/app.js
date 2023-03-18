ad4a946c746d53ee2e4c889c3455adb2b70c82c1
Ade1fe <addypearl09@gmail.com>
2023-03-18 18:49:15 +0100

    Finally Done

--- Restuarant/js/app.js
+++ Restuarant/js/app.js
@@ -24,43 +24,6 @@
 
 
 
-
-// counter
-
-var counted = 0;
-$(window).scroll(function() {
-
-  var oTop = $('#counter').offset().top - window.innerHeight;
-  if (counted == 0 && $(window).scrollTop() > oTop) {
-    $('.count').each(function() {
-      var $this = $(this),
-        countTo = $this.attr('data-count');
-      $({
-        countNum: $this.text()
-      }).animate({
-          countNum: countTo
-        },
-
-        {
-
-          duration: 2000,
-          easing: 'swing',
-          step: function() {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function() {
-            $this.text(this.countNum);
-            //alert('finished');
-          }
-
-        });
-    });
-    counted = 1;
-  }
-
-});
-
-
 // owl carousel
    $(document).ready(function() {
  
@@ -77,6 +40,10 @@ $(window).scroll(function() {
 });
 
 
+const price = document.querySelector('.price');
+price.addEventListener("click", ()=>{
+  alert('Coming Soon...')
+})
 
 
 
