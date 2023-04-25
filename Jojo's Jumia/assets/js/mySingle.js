982256a6ab9a8ac664aa0292f7df124f3bafae01
Ade1fe <addypearl09@gmail.com>
2023-04-25 18:40:30 +0100

    testing

---
+++ Jojo's Jumia/assets/js/mySingle.js
@@ -0,0 +1,8 @@
+ var firstSwipper = new Swiper(".firstSwipper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
