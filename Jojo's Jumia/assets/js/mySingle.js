6b66b3a5ef20559d33b493428f6690ee9b370eb1
Ade1fe <miraadams08@gamil.com>
2023-04-26 12:53:20 +0100

    testing

--- Jojo's Jumia/assets/js/mySingle.js
+++ Jojo's Jumia/assets/js/mySingle.js
@@ -1,8 +1,10 @@
  var firstSwipper = new Swiper(".firstSwipper", {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+     slidesPerView: 3,
+     spaceBetween: 30,
+     pagination: {
+         el: ".swiper-pagination",
+         clickable: true,
+     },
+ });
+
+ console.log("hello");
