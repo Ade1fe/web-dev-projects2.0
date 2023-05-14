419befc1eeb542612c7d4c9242505d04fd3a43e2
Ade1fe <addypearl09@gmail.com>
2023-05-14 21:20:02 +0100

    I have finally fixed

--- Manga Wbsite/js/index.js
+++ Manga Wbsite/js/index.js
@@ -93,93 +93,93 @@ menuTwo.addEventListener("click", () => {
 
 
 // my adverts
-const pixabayApiKey = '34748321-56ec586673804760cca13f7f6';
-const pixabayBaseUrl = 'https://pixabay.com/api';
-const pixabayEndpoint = `${pixabayBaseUrl}/?key=${pixabayApiKey}&q=job&image_type=photo&per_page=40`;
+// const pixabayApiKey = '34748321-56ec586673804760cca13f7f6';
+// const pixabayBaseUrl = 'https://pixabay.com/api';
+// const pixabayEndpoint = `${pixabayBaseUrl}/?key=${pixabayApiKey}&q=job&image_type=photo&per_page=40`;
 
-const adzunaApiKey = '02786c33c3cf2d6b3ed4996c322f3e3f';
-const adzunaBaseUrl = 'https://api.adzuna.com/v1';
-const adzunaEndpoint = `${adzunaBaseUrl}/api/jobs/gb/search/1`;
-const adzunaQueryParams = {
-  app_id: '9f5cec5c',
-  app_key: adzunaApiKey,
-  results_per_page: 40, // Adjust the number of jobs per page as desired
-};
+// const adzunaApiKey = '02786c33c3cf2d6b3ed4996c322f3e3f';
+// const adzunaBaseUrl = 'https://api.adzuna.com/v1';
+// const adzunaEndpoint = `${adzunaBaseUrl}/api/jobs/gb/search/1`;
+// const adzunaQueryParams = {
+//   app_id: '9f5cec5c',
+//   app_key: adzunaApiKey,
+//   results_per_page: 40, // Adjust the number of jobs per page as desired
+// };
 
-const advertContainer = document.getElementById('advert-container');
+// const advertContainer = document.getElementById('advert-container');
 
-fetch(pixabayEndpoint)
-  .then((response) => response.json())
-  .then((pixabayData) => {
-    const imageList = pixabayData.hits;
+// fetch(pixabayEndpoint)
+//   .then((response) => response.json())
+//   .then((pixabayData) => {
+//     const imageList = pixabayData.hits;
 
-    fetchAdzunaJobs(imageList);
-  })
-  .catch((error) => {
-    console.error('Error occurred:', error);
-  });
+//     fetchAdzunaJobs(imageList);
+//   })
+//   .catch((error) => {
+//     console.error('Error occurred:', error);
+//   });
 
-function fetchAdzunaJobs(imageList) {
-  const adzunaQueryString = Object.keys(adzunaQueryParams)
-    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(adzunaQueryParams[key])}`)
-    .join('&');
-  const adzunaUrl = `${adzunaEndpoint}?${adzunaQueryString}`;
+// function fetchAdzunaJobs(imageList) {
+//   const adzunaQueryString = Object.keys(adzunaQueryParams)
+//     .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(adzunaQueryParams[key])}`)
+//     .join('&');
+//   const adzunaUrl = `${adzunaEndpoint}?${adzunaQueryString}`;
 
-  fetch(adzunaUrl)
-    .then((response) => response.json())
-    .then((adzunaData) => {
-      const jobList = adzunaData.results;
-      let currentIndex = 0;
+//   fetch(adzunaUrl)
+//     .then((response) => response.json())
+//     .then((adzunaData) => {
+//       const jobList = adzunaData.results;
+//       let currentIndex = 0;
 
-      function changeAdvert() {
-        const job = jobList[currentIndex];
-        const image = imageList[currentIndex];
+//       function changeAdvert() {
+//         const job = jobList[currentIndex];
+//         const image = imageList[currentIndex];
 
-        const advertPost = document.createElement('div');
-        advertPost.classList.add('advert-post');
+//         const advertPost = document.createElement('div');
+//         advertPost.classList.add('advert-post');
 
-        const advertImage = document.createElement('img');
-        advertImage.src = image.webformatURL;
-        advertImage.alt = 'Advert Image';
+//         const advertImage = document.createElement('img');
+//         advertImage.src = image.webformatURL;
+//         advertImage.alt = 'Advert Image';
 
-        const advertDetails = document.createElement('div');
-        advertDetails.classList.add('advert-details');
+//         const advertDetails = document.createElement('div');
+//         advertDetails.classList.add('advert-details');
 
-        const advertTitle = document.createElement('h2');
-        advertTitle.classList.add('advert-title');
-        advertTitle.textContent = job.title;
+//         const advertTitle = document.createElement('h2');
+//         advertTitle.classList.add('advert-title');
+//         advertTitle.textContent = job.title;
 
-        const jobDescription = document.createElement('p');
-        jobDescription.classList.add('advert-description');
-        jobDescription.textContent = job.description;
+//         const jobDescription = document.createElement('p');
+//         jobDescription.classList.add('advert-description');
+//         jobDescription.textContent = job.description;
 
-        if (jobDescription.textContent.length > 60) {
-          jobDescription.textContent = jobDescription.textContent.slice(0, 60) + '...';
-        }
+//         if (jobDescription.textContent.length > 60) {
+//           jobDescription.textContent = jobDescription.textContent.slice(0, 60) + '...';
+//         }
 
-        const readMoreLink = document.createElement('a');
-        readMoreLink.href = job.redirect_url;
-        readMoreLink.classList.add('btn');
-        readMoreLink.textContent = 'Read More';
+//         const readMoreLink = document.createElement('a');
+//         readMoreLink.href = job.redirect_url;
+//         readMoreLink.classList.add('btn');
+//         readMoreLink.textContent = 'Read More';
 
-        advertDetails.appendChild(advertTitle);
-        advertDetails.appendChild(jobDescription);
-        advertDetails.appendChild(readMoreLink);
+//         advertDetails.appendChild(advertTitle);
+//         advertDetails.appendChild(jobDescription);
+//         advertDetails.appendChild(readMoreLink);
 
-        advertPost.appendChild(advertImage);
-        advertPost.appendChild(advertDetails);
+//         advertPost.appendChild(advertImage);
+//         advertPost.appendChild(advertDetails);
 
-        advertContainer.innerHTML = ''; // Clear existing advertisement
-        advertContainer.appendChild(advertPost);
+//         advertContainer.innerHTML = ''; // Clear existing advertisement
+//         advertContainer.appendChild(advertPost);
 
-        currentIndex = (currentIndex + 1) % jobList.length; // Update current index for the next advertisement
+//         currentIndex = (currentIndex + 1) % jobList.length; // Update current index for the next advertisement
 
-        setTimeout(changeAdvert, 3000); // Schedule the next advertisement change after 3 seconds
-      }
+//         setTimeout(changeAdvert, 3000); // Schedule the next advertisement change after 3 seconds
+//       }
 
-      setTimeout(changeAdvert, 3000); // Start the advertisement rotation after 3 seconds
-    })
-    .catch((error) => {
-      console.error('Error occurred:', error);
-    });
-}
+//       setTimeout(changeAdvert, 3000); // Start the advertisement rotation after 3 seconds
+//     })
+//     .catch((error) => {
+//       console.error('Error occurred:', error);
+//     });
+// }
